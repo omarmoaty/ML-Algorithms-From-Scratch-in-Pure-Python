01b02c37458ec50d022cdcefb27eeee2cca760bc
omarmoaty <omarmoaty2002@gmail.com>
2026-01-15 14:44:38 +0200

    Implement gradient descent training for linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -9,9 +9,22 @@ class LinearRegression:
 		self.bias = None
 	
 	def fit(self, x, y):
-		n_features = x.shape[1]
+		n_samples, n_features = x.shape
 		self.weights = np.zeros(n_features)
 		self.bias = 0.0
+
+		for _ in range(self.epochs):
+			y_pred = self.predict(x)
+
+			dw = (1/n_samples) * np.dot(x.t, (y_pred - y))
+			db = (1/n_samples) * np.sum(y_pred - y)
+
+			self.weights -= self.learning_rate * dw
+			self.bias -= self.learning_rate * db
 	
 	def predict(self, x):
-		return np.dot(x,self.weights) + self.bias
+		return np.dot(x,self.weights) + self.bias 
+	
+	def _mse(self,y_true, y_pred):
+		return np.mean((y_true-y_pred)**2)
+	
