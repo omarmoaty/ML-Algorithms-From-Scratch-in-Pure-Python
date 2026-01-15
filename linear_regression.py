1d699bc65c59b31e13095697976a739498fee211
omarmoaty <omarmoaty2002@gmail.com>
2026-01-15 13:36:37 +0200

    Add LinearRegression initialization parameters

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,9 @@
 #this file implements Linear Regression from scratch using gradient descent and mean-squared-error
+import random
 class LinearRegression:
-	pass
+	def __init__(self, learning_rate = 0.01, epochs = 1000):
+		self.learning_rate = learning_rate
+		self.epochs = epochs
+		self.weights = None
+		self.bias = None
 	
