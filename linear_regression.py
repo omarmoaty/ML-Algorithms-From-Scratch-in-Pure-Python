72b9848d2554fefb55c945beb3706918046e6cf6
omarmoaty <omarmoaty2002@gmail.com>
2026-01-15 13:44:34 +0200

    Initialize weights and bias in fit method

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,6 @@
 #this file implements Linear Regression from scratch using gradient descent and mean-squared-error
 import random
+import numpy as np
 class LinearRegression:
 	def __init__(self, learning_rate = 0.01, epochs = 1000):
 		self.learning_rate = learning_rate
@@ -7,3 +8,7 @@ class LinearRegression:
 		self.weights = None
 		self.bias = None
 	
+	def fit(self, x, y):
+		n_features = x.shape[1]
+		self.weights = np.zeros(n_features)
+		self.bias = 0.0
