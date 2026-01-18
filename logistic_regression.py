8dcb90b35398a8b3b457d4976e87c66633ec7fc5
omarmoaty <omarmoaty2002@gmail.com>
2026-01-18 14:22:11 +0200

    Add logistic regression skeleton and sigmoid function

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,1 +1,15 @@
+import numpy as np
 
+class Logestic_Regression:
+    def __init__(self, learning_rate=0.01, epochs=1000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+
+    def _sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+    
+    def predict_probability(self, x):
+        linear_output = np.dot(x, self.weights) + self.bias
+        return self._sigmoid(linear_output)
